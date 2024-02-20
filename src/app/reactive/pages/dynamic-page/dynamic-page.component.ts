efad2fb494f53cd234b3dc15765d928fb731fab1
tavodevcode <tavodevcode@gmail.com>
2024-02-19 22:26:27 -0600

    📄 Add form validation and reset functionality to DynamicPageComponent

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
 export class DynamicPageComponent {
+  public myform: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear Solid', Validators.required],
+      ['The Legend of Zelda', Validators.required],
+      ['Final Fantasy', Validators.required]
+    ])
+  })
 
+
+  public get favoriteGames(): FormArray {
+    return this.myform.get('favoriteGames') as FormArray
+  }
+
+
+  constructor(private fb: FormBuilder) { }
+
+  onSubmit() {
+    if (this.myform.invalid) {
+      // return this.myform.markAllAsTouched()
+    }
+
+    console.log(this.myform.value)
+
+    this.myform.reset()
+  }
 }
